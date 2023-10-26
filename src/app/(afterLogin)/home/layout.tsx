39d1a5a6c3910c8a83dd15ec39757404858db354
김지환 <jihwan-morning@mz.co.kr>
2023-10-26 18:39:44 +0900

    directory setup

--- src/app/(afterLogin)/home/layout.tsx
+++ src/app/(afterLogin)/home/layout.tsx
@@ -1,9 +1,17 @@
-export default async function HomeLayout({children}): Promise<Element> {
+// export default async function HomeLayout({children}): Promise<Element> {
+//   return(
+//     <div>
+//       홈 레이아웃
+//       {children}</div>
+//   );
+// }
+
+// // RootLayout -> HomeLayout -> Home
+
+export default async function HomeLayout({children}){
   return(
-    <div>
-      홈 레이아웃
-      {children}</div>
+      <div>
+          홈 레이아웃
+          {children}</div>
   );
 }
-
-// RootLayout -> HomeLayout -> Home
